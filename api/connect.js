c9eb1ce212bb3fbda10b458135c520da73cd5ace
Archit Mahule <mahulearchit@gmail.com>
2023-08-25 21:59:03 +0530

    first commit

--- api/connect.js
+++ api/connect.js
@@ -3,6 +3,23 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
   host:"localhost",
   user:"root",
-  password:"Lamadev123",
+  password:"arc_4321",
   database:"social"
 })
+
+// db.connect(function (err) {
+//   if (err) {
+//       console.log("Error in the connection")
+//       console.log(err)
+//   }
+//   else {
+//       console.log(`Database Connected`)
+//       connection.query(`SHOW DATABASES`,
+//           function (err, result) {
+//               if (err)
+//                   console.log(`Error executing the query - ${err}`)
+//               else
+//                   console.log("Result: ", result)
+//           })
+//   }
+// })
--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+
 app.use(express.json());
 app.use(
   cors({
